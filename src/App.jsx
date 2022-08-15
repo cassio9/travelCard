c2ddb9fa420927babfc179ebce697c01fee012c9
Cassio Soares <cassiosoares9@hotmail.com>
2022-08-15 16:43:56 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import TravelMoments from './components/TravelMoments'
+import data from "./data"
+
+const DataTravel = data.map(elem => {
+  return (
+      <TravelMoments 
+          key={elem.id}
+          {...elem}
+      />
+  )
+    
+})
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <section className='travelCard-container'>
+        {DataTravel}
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar () {
+    return ( 
+        <div className="nav-container">
+            <p className='title'>my travel journal.</p>
+        </div>
+     );
+};
